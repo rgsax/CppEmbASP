f1b26638f1cedd1d249493a85e9d6dfcf53ae7e8
Riccardo Giordano <riccardog.sax@gmail.com>
2019-02-16 19:03:42 +0100

    improved CmdExecutor impementation with a more powerful Boost.Process library

--- CmdExecutor.hpp
+++ CmdExecutor.hpp
@@ -2,13 +2,13 @@
 #define CMDEXECUTOR_H
 
 /*
- * Compile using the following flags: -std=c++17 -lstdc++fs
+ * Compile using the following flags: -lboost_system
  */
 
-#include <boost/filesystem.hpp> //C++17 and newer versions
-#include <cstdlib>
-#include <fstream>
+#include <boost/process.hpp>
 #include <stdexcept>
+
+namespace boost_process = boost::process;
 
 class CmdExecutor {
 public:
@@ -20,10 +20,16 @@ public:
 	}
 
 	int execute(std::string command) {
-		command += " > " + output_path + " 2> " + error_output_path; //The output will be redirected to the temporary files
-		int status = std::system(command.c_str());
+		boost_process::ipstream *out = new boost_process::ipstream;
+		boost_process::ipstream *err = new boost_process::ipstream;
 
-		collectOutput();
+		int status = boost_process::system(command, boost_process::std_out > (*out), boost_process::std_err > (*err));
+
+		delete output_stream;
+		delete error_output_stream;
+
+		output_stream = out;
+		error_output_stream = err;
 
 		return status;
 	}
@@ -49,35 +55,12 @@ public:
 
 private:
 	static CmdExecutor *instance;
-	static const std::string OUTPUT_FILENAME;
-	static const std::string ERROR_OUTPUT_FILENAME;
 
     std::istream *output_stream = nullptr;
     std::istream *error_output_stream = nullptr;
 
-	std::string output_path;
-	std::string error_output_path;
-
-
-	void collectOutput() {
-		delete output_stream; //Prevent memory leak
-		delete error_output_stream;
-		output_stream = new std::ifstream(output_path); //Open the temporary file containing the output of the command using a stream
-        error_output_stream = new std::ifstream(error_output_path); //Open the temporary file containing the error output of the command using a stream
-	}
-
-	CmdExecutor() {
-        std::string tmpDir = boost::filesystem::temp_directory_path().string();
-		output_path = tmpDir + "/" + OUTPUT_FILENAME; //The temporary file for the output
-        error_output_path = tmpDir + "/" + ERROR_OUTPUT_FILENAME;
-	}
-
-	CmdExecutor(const CmdExecutor &c) {	}
-
 };
 
 CmdExecutor* CmdExecutor::instance = nullptr;
-const std::string CmdExecutor::OUTPUT_FILENAME = "cmd_executor_output";
-const std::string CmdExecutor::ERROR_OUTPUT_FILENAME = "cmd_executor_error_output";
 
 #endif
